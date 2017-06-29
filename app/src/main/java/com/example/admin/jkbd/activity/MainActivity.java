0e4faf4d08a23871a3c687ab76998146341878ed
dangdang <1941700435@qq.com>
2017-06-30 00:15:13 +0800

    点击退出事件

--- app/src/main/java/com/example/admin/jkbd/activity/MainActivity.java
+++ app/src/main/java/com/example/admin/jkbd/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.admin.jkbd.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.admin.jkbd.R;
 
@@ -10,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.random_test);
+        setContentView(R.layout.activity_main);
+    }
+    public void text(View view){
+        startActivity(new Intent(MainActivity.this,rand.class));
+
+    }
+    public void exit(View view){
+        finish();
+
     }
 }
