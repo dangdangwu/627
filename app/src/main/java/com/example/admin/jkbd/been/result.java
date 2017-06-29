3cdf75207c79abb6e9eba07cd678fb4775587d1b
dangdang <1941700435@qq.com>
2017-06-29 22:31:50 +0800

    三个实体类

---
+++ app/src/main/java/com/example/admin/jkbd/been/result.java
@@ -0,0 +1,46 @@
+package com.example.admin.jkbd.been;
+
+import java.util.List;
+
+/**
+ * Created by gg on 2017/6/29.
+ */
+
+public class result {
+
+
+    /**
+     * error_code : 0
+     * reason : ok
+     * result : [{"id":30,"question":"这个标志是何含义？","answer":"2","item1":"十字交叉路口预告","item2":"互通式立体交叉预告","item3":"Y型交叉路口预告","item4":"环行交叉路口预告","explains":"互通式立体交叉：路线交叉中的一种重要类型。交叉道路之间立体交叉并以匝道相互连通以实现交通转换的交叉。","url":"http://images.juheapi.com/jztk/c1c2subject1/30.jpg"}]
+     */
+
+    private int error_code;
+    private String reason;
+    private List<Question> result;
+
+    public int getError_code() {
+        return error_code;
+    }
+
+    public void setError_code(int error_code) {
+        this.error_code = error_code;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public List<Question> getResult() {
+        return result;
+    }
+
+    public void setResult(List<Question> result) {
+        this.result = result;
+    }
+
+}
