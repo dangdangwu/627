14eafcf0088c1eeb493c34167b774b6056199279
dangdang <1941700435@qq.com>
2017-06-29 21:22:16 +0800

    将activity放到对应的包里

--- app/src/main/java/com/example/admin/jkbd/activity/MainActivity.java
+++ app/src/main/java/com/example/admin/jkbd/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.admin.jkbd;
+package com.example.admin.jkbd.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.admin.jkbd.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/admin/jkbd/activity/SplashActivity.java
@@ -0,0 +1,8 @@
+package com.example.admin.jkbd.activity;
+
+/**
+ * Created by admin on 2017/6/29.
+ */
+
+public class SplashActivity {
+}
