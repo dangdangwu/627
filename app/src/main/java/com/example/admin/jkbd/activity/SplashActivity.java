3e668e62d4f023692c2d211f444973fba33bf7bf
dangdang <1941700435@qq.com>
2017-06-29 22:11:23 +0800

    闪屏和退出

--- app/src/main/java/com/example/admin/jkbd/activity/SplashActivity.java
+++ app/src/main/java/com/example/admin/jkbd/activity/SplashActivity.java
@@ -1,8 +1,36 @@
 package com.example.admin.jkbd.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.admin.jkbd.R;
+
 /**
  * Created by admin on 2017/6/29.
  */
 
-public class SplashActivity {
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shan);
+        afs.start();
+    }
+    CountDownTimer afs=new CountDownTimer(2000,1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            SplashActivity.this.finish();
+
+        }
+    };
+
 }
